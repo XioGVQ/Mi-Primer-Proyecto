b249c9a3d0a7d0fa477ac908c69d02ce86f13de9
XioGVQ <163697353+XioGVQ@users.noreply.github.com>
2025-10-23 18:31:04 -0600

    Add files via upload
    Agregando el primer proyecto en html, css y js.

---
+++ 01_Prueba.js
@@ -0,0 +1,9 @@
+
+function AlertUI() {   
+        alert ("Esto es una alerta asi q no se");
+}
+
+function EntradaBoton2() {
+        entradaUsuario = prompt("Escribe algo aqui"); // Esto es para la entrada del usuario
+        alert (entradaUsuario);
+}
